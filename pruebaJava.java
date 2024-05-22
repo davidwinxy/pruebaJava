0645377d5657bc5f314b13a1535d8b365bcc9279
katherine29Nicole <kf562739@gmail.com>
2024-05-22 14:29:04 -0600

    commit de nicol

--- pruebaJava.java
+++ pruebaJava.java
@@ -1,4 +1,7 @@
 public class pruebaJava {
     public void commitDavid(){
         System.out.printf("commit de david"); }
+
+    public void commitNicole(){
+        System.out.printf("commit de Nicole"); }
 }
