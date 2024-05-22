54628b62d0f7379aa5f62f4f359a210d9b9476f1
anderson109 <xxandersoncatxx@gmail.com>
2024-05-22 14:35:06 -0600

    commit de Anderson

--- pruebaJava.java
+++ pruebaJava.java
@@ -4,4 +4,7 @@ public class pruebaJava {
 
     public void commitNicole(){
         System.out.printf("commit de Nicole"); }
+
+    public void commitAnderson(){
+        System.out.printf("commit de Anderson"); }
 }
